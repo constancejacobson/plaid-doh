8640a83020c5aaf8ad7e61824a65497cd9f9d4d8
cjacobson <constance.jacobson@xe.com>
2020-07-01 22:46:02 -0400

    Initial Commit

---
+++ plaid-doh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  plaid-doh
+//
+//  Created by Constance Jacobson on 2020-07-01.
+//  Copyright © 2020 Constance Jacobson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ plaid-dohTests/plaid_dohTests.swift
@@ -0,0 +1,34 @@
+//
+//  plaid_dohTests.swift
+//  plaid-dohTests
+//
+//  Created by Constance Jacobson on 2020-07-01.
+//  Copyright © 2020 Constance Jacobson. All rights reserved.
+//
+
+import XCTest
+@testable import plaid_doh
+
+class plaid_dohTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
